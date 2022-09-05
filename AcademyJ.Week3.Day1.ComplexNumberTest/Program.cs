b25d6ed9c1227ef2ec99ca1dce6b6f8e20aedffa
Davide Maggiulli <davide.maggiulli@gmail.com>
2022-09-05 16:04:36 +0200

    Complex Number

--- AcademyJ.Week3.Day1.ComplexNumberTest/Program.cs
+++ AcademyJ.Week3.Day1.ComplexNumberTest/Program.cs
@@ -8,8 +8,37 @@ namespace AcademyJ.Week3.Day1.ComplexNumberTest
         {
             Console.WriteLine("** Complex Number test **");
             ComplexNumber c1 = ReadComplexNumberFromConsole();
-            ComplexNumber c2 = ReadComplexNumberFromConsole();
+            Console.WriteLine(c1);
 
+            ComplexNumber c2 = ReadComplexNumberFromConsole();
+            Console.WriteLine(c2);
+
+
+            Console.WriteLine("Quale operazione vuoi svolgere (+, -, *, /):");
+            string op = Console.ReadLine();
+            if(op == "+")
+            {
+                Console.WriteLine($"Somma tra {c1} e {c2}: {c1.Add(c2)}");
+            }
+            else if (op == "-")
+            {
+                Console.WriteLine($"Differenza tra {c1} e {c2}: {c1.Sub(c2)}");
+            }
+            else if (op == "*")
+            {
+                Console.WriteLine($"Prodotto tra {c1} e {c2}: {c1.Mul(c2)}");
+            }
+            else if (op == "/")
+            {
+                Console.WriteLine($"Divisione tra {c1} e {c2}: {c1.Div(c2)}");
+            }
+            else
+            {
+                Console.WriteLine("Operazione non valida.");
+            }
+
+
+            Console.ReadLine();
         }
 
         private static ComplexNumber ReadComplexNumberFromConsole()
@@ -21,9 +50,9 @@ namespace AcademyJ.Week3.Day1.ComplexNumberTest
             
 
             Console.Write("Parte immaginaria:\t");
-            success = double.TryParse(Console.ReadLine(), out double i);
+            success = double.TryParse(Console.ReadLine(), out double imaginary);
 
-            return new ComplexNumber();  //TODO COMPLETE
+            return new ComplexNumber(real,imaginary);  
         }
     }
 }
--- AcademyJ.Week3.Day1.Lib/ComplexNumber.cs
+++ AcademyJ.Week3.Day1.Lib/ComplexNumber.cs
@@ -8,5 +8,74 @@ namespace AcademyJ.Week3.Day1.Lib
 {
     public class ComplexNumber
     {
+        public double Real { get;}
+        public double Imaginary { get;}
+
+        //public ComplexNumber Conjugated
+        //{
+        //    get
+        //    {
+        //        return new ComplexNumber(Real, -Imaginary);
+        //    }
+        //}
+        public ComplexNumber Conjugated => new ComplexNumber(Real, -Imaginary);
+
+        public double Module => Math.Sqrt(Real * Real + Imaginary * Imaginary);
+
+
+        public ComplexNumber(double real, double imaginary)
+        {
+            Real = real;
+            Imaginary = imaginary;
+        }
+
+        public ComplexNumber() : this(0,0)
+        {
+            //Real = 0;
+            //Imaginary = 0;
+        }
+
+        public ComplexNumber(double real) : this(real,0)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            if (Real == 0 && Imaginary == 0)
+                return "0";
+
+            if (Real == 0)
+                return $"{Imaginary}i";
+
+            if(Imaginary == 0)
+                return $"{Real}";
+
+            if(Imaginary < 0)
+                return $"{Real} - {Math.Abs(Imaginary)}i";
+
+            return $"{Real} + {Imaginary}i";
+        }
+
+        public ComplexNumber Add(ComplexNumber c)
+        {
+            return new ComplexNumber(this.Real + c.Real, Imaginary + c.Imaginary);
+        }
+
+        public ComplexNumber Sub(ComplexNumber c)
+        {
+            return new ComplexNumber(this.Real - c.Real, Imaginary - c.Imaginary);
+        }
+
+        public ComplexNumber Mul(ComplexNumber c)
+        {
+            return new ComplexNumber(this.Real*c.Real - Imaginary*c.Imaginary, Real * c.Imaginary + Imaginary * c.Real);
+        }
+
+        public ComplexNumber Div(ComplexNumber c)
+        {
+            double den = Math.Pow(c.Real, 2) + Math.Pow(c.Imaginary, 2);
+            return new ComplexNumber((Real * c.Real + Imaginary * c.Imaginary)/den, (Real * c.Imaginary - Imaginary * c.Real)/den);
+        }
     }
 }
